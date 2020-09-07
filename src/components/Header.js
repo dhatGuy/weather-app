74b53f0bd67ed90bcaac1b5b76f50ba79eec837b
dhatGuy <odunsiolakunbi@gmail.com>
2020-09-07 11:54:24 +0100

    Header

---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import icon from "../assets/icon.svg";
+const Icon = styled.img``;
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #00005c;
+  position: relative;
+  height: 50px;
+`;
+
+const H1 = styled.span`
+  font-weight: bold;
+  font-size: 21px;
+  color: #ffdcb4;
+`;
+
+const Header = (props) => {
+  return (
+    <>
+      <Container>
+        <Icon src={icon} alt="icon" />
+        <H1>WEATHER FORECAST</H1>
+      </Container>
+    </>
+  );
+};
+
+export default Header;
